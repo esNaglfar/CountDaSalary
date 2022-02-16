1a48729c779a5a5a1abbd38f4fae1339fe702326
esNaglfar <60138899+esNaglfar@users.noreply.github.com>
2022-02-16 23:34:08 +0300

    some improvements

--- CountDaSalary/Source.cpp
+++ CountDaSalary/Source.cpp
@@ -7,70 +7,57 @@
 
 using namespace std;
 
-
-/// <summary>
-/// Обычный класс для студентиков с парой полей, функцией печати и гетерами
-/// </summary>
 class Student
 {
 public:
-	Student(string& _name, float _salary, float _bonus)
+	Student(string& _name, double _salary, double _bonus)
 		: name(_name), salary(_salary), bonus(_bonus) {}
 
 	string& getName() { return name; }
-	float getSalary() { return salary; }
-	float getBonus() { return bonus; }
+	double getSalary() { return salary; }
+	double getBonus() { return bonus; }
 
-	void PrintData()
+	double getFullSalary() { return salary + salary * bonus / 100; }
+
+	string GetStudentData()
 	{
-		cout << "\nStudent: " + name + "\nSalary: " + to_string(salary) + "\nBonus: " + to_string(bonus)+"\n==========\n";
+		
+		return "\nStudent: " + name + "\nSalary:" + to_string(salary) + "\nBonus: " + to_string(bonus)+"%\n==========\nTotal salary: " + to_string(getFullSalary()) + "\n";
 	}
 
-protected:
+private:
 	string name;
-	float salary;
-	float bonus;
+	double salary;
+	double bonus;
 };
 
-/// <summary>
-/// Функция чтения данных из файла. Возвращает распаршенные данные в виде массива студентиков
-/// </summary>
-/// <param name="_fileName"></param>
-/// <returns></returns>
 vector<Student> ReadStudentFile(string&& _fileName)
 {
-
-	//filesystem::path path = filesystem::current_path().u8string() + "\\" + _fileName;
-	//cout << path;
-	
-
 	ifstream file(_fileName);
-
 	vector<Student> vec;
-
 	string separator = ";";
 	
-
-
 	if (file.is_open())
 	{
 		string line;
+
 		while (getline(file, line))
 		{
-			string name = line.substr(0, line.find(separator));  // Самоя простая часть. Взяли строку до первого вхождения разделителя.
-			string other = line.substr(name.length()+1, line.length()-name.length());  // пошли макароны. По сути, мы из исходной строки убрали всё что было до первого разделителя + сам разделитель
-			string salary_text = other.substr(0, other.find(separator)); // из полученного остатка забираем занечния до первого вхождения разделителя
-			other = other.substr(salary_text.length() + 1, other.length() - name.length()); // повторяем процедуру разбиения строки. Вырезаем уже два куска.
-			string bonus_text = other.substr(0, other.find(separator)); // Забираем последние данные
+			string name = line.substr(0, line.find(separator));  
+			string other = line.substr(name.length()+1, line.length()-name.length());  
+			string salary_text = other.substr(0, other.find(separator)); 
+			other = other.substr(salary_text.length() + 1, other.length() - name.length());
+			string bonus_text = other.substr(0, other.find(separator));
 
+			double salary = stod(salary_text);
+			double bonus = stod(bonus_text);
 
-			float salary = stof(salary_text); // привели к флоату
-			float bonus = stof(bonus_text);
-
-			Student s(name, salary, bonus); // создали объект и запихнули в массив
+			Student s(name, salary, bonus); 
 			vec.push_back(s);
 		}
+
 		file.close();
+
 	}
 	return vec;
 }
@@ -87,6 +74,6 @@ int main()
 
 	for(auto _s : s)
 	{
-		_s.PrintData();
+		cout << _s.GetStudentData();
 	}
 }
